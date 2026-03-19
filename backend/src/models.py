0b9f79bccad35ac50b62e543a203849a1d5f020e
Ben Dalton <bennd2007@gmail.com>
2026-03-19 14:35:26 +0000

    update models to work with dates - now in line with ERD.png

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column, relationship, MappedAsDataclass, DeclarativeBase
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, func
 from typing import List
+from datetime import datetime
 
 class Base(DeclarativeBase, MappedAsDataclass):
     pass
@@ -15,7 +16,7 @@ class UserCommunityTask(Base):
 
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), primary_key=True, init=True)
     community_task_id: Mapped[int] = mapped_column(ForeignKey("community_task.id", ondelete="CASCADE"), primary_key=True, init=True)
-    completed: Mapped[bool] = mapped_column(default=False, init=False)
+    date_completed: Mapped[datetime] = mapped_column(server_default=func.now()) # automatically stamps when the record is created
     
     user: Mapped["User"] = relationship(back_populates="user_community_tasks", init=False)
     community_task: Mapped["CommunityTask"] = relationship(back_populates="user_community_tasks", init=False)
@@ -24,19 +25,15 @@ class UserCommunityTask(Base):
         return {
             "user_id": self.user_id,
             "community_task_id": self.community_task_id,
-            "completed": self.completed,
-            "description": self.community_task.description
+            "date_completed": self.date_completed
         }
 
 
 class CommunityUnlockable(Base):
     __tablename__ = "community_unlockable"
 
-    # records are created when the unlockable is purchase, so unlocked is always True (an oversight in DB design.)
-
     community_id: Mapped[int] = mapped_column(ForeignKey("community.id", ondelete="CASCADE"), primary_key=True, init=True)
     unlockable_id: Mapped[int] = mapped_column(ForeignKey("unlockable.id", ondelete="CASCADE"), primary_key=True, init=True)
-    unlocked: Mapped[bool] = mapped_column(default=True, init=False)
     applied: Mapped[bool] = mapped_column(default=False, init=False)
 
     community: Mapped["Community"] = relationship(back_populates="community_unlockables", init=False)
@@ -46,13 +43,13 @@ class CommunityUnlockable(Base):
         return {
             "community_id": self.community_id,
             "unlockable_id": self.unlockable_id,
-            "unlocked": self.unlocked,
             "applied": self.applied,
             "unlockable": self.unlockable.to_dict() # not circular
         }
     
 
 # main entities
+
 
 class User(Base):
     __tablename__ = "user"
@@ -77,8 +74,6 @@ class User(Base):
             "balance": self.balance,
             "admin": self.admin,
             "community_id": self.community_id,
-            "user_tasks": [task.to_dict() for task in self.user_tasks], # not circular
-            "user_community_tasks": [task.to_dict() for task in self.user_community_tasks] # not circular
         }
 
 
@@ -89,7 +84,9 @@ class UserTask(Base):
     description: Mapped[str] = mapped_column(init=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), init=True)
     completed: Mapped[bool] = mapped_column(default=False, init=False)
-    
+    created_date: Mapped[datetime] = mapped_column(server_default=func.now())
+    completed_date: Mapped[datetime] = mapped_column(init=False)
+
     user: Mapped["User"] = relationship(back_populates="user_tasks", init=False)
 
     def to_dict(self):
@@ -97,7 +94,9 @@ class UserTask(Base):
             "id": self.id,
             "description": self.description,
             "user_id": self.user_id,
-            "completed": self.completed
+            "completed": self.completed,
+            "created_date": self.created_date,
+            "completed_date": self.completed_date
         }
 
 
@@ -108,7 +107,6 @@ class Community(Base):
     name: Mapped[str] = mapped_column(init=True)
     tier_progress: Mapped[float] = mapped_column(default=0.0, init=False)
     tier: Mapped[int] = mapped_column(default=0, init=False)
-    participation: Mapped[float] = mapped_column(default=0.0, init=False)
 
     users: Mapped[List["User"]] = relationship(back_populates="community", init=False)
     community_tasks: Mapped[List["CommunityTask"]] = relationship(back_populates="community", cascade="all, delete-orphan", init=False)
@@ -120,9 +118,7 @@ class Community(Base):
             "name": self.name,
             "tier_progress": self.tier_progress,
             "tier": self.tier,
-            "participation": self.participation,
             "user_usernames": [user.username for user in self.users],
-            "unlockables": [unlockable.to_dict() for unlockable in self.community_unlockables] # not circular
         }
 
 
@@ -132,7 +128,8 @@ class CommunityTask(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
     description: Mapped[str] = mapped_column(init=True)
     community_id: Mapped[int] = mapped_column(ForeignKey("community.id", ondelete="CASCADE"), init=True)
-    
+    created_date: Mapped[datetime] = mapped_column(server_default=func.now())
+
     community: Mapped["Community"] = relationship(back_populates="community_tasks", init=False)
     user_community_tasks: Mapped[List["UserCommunityTask"]] = relationship(back_populates="community_task", cascade="all, delete-orphan", init=False)
 
@@ -140,26 +137,25 @@ class CommunityTask(Base):
         return {
             "id": self.id,
             "description": self.description,
-            "community_id": self.community_id
+            "community_id": self.community_id,
+            "created_date": self.created_date
         }
 
     
 class Unlockable(Base):
     __tablename__ = "unlockable"
 
-    # currently only allows for a single type of unlockable (type of plant), as there is no 'type' field
-
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
-    name: Mapped[str] = mapped_column(init=True)
-    description: Mapped[str] = mapped_column(init=True)
     price: Mapped[int] = mapped_column(init=True)
+    category: Mapped[str] = mapped_column(init=True)
+    minimum_tier: Mapped[int] = mapped_column(init=True)
 
     community_unlockables: Mapped[List["CommunityUnlockable"]] = relationship(back_populates="unlockable", cascade="all, delete-orphan", init=False)
 
     def to_dict(self):
         return {
             "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "price": self.price
+            "price": self.price,
+            "category": self.category,
+            "minimum_tier": self.minimum_tier
         }
